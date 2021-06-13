162fe85315d7addee9eb6ea2b88b4d8dbbccd126
Chen Shalev <chen890890@gmail.com>
2021-06-13 14:46:40 +0300

    fix footer

---
+++ JS/firebase.js
@@ -0,0 +1,15 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDWDalX7BulETbJpLeFXsEgYvXH1_lBSwY",
+    authDomain: "esploro-support-tool.firebaseapp.com",
+    projectId: "esploro-support-tool",
+    storageBucket: "esploro-support-tool.appspot.com",
+    messagingSenderId: "901304481903",
+    appId: "1:901304481903:web:975952afeaf1278f4bf59a",
+    measurementId: "G-WNW3TJYH0R"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
